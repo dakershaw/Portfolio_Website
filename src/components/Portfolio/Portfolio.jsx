a0a1647ad6b5a0e69bc4a9284c249262f6638b01
hamidayb <hayub.bese18seecs@seecs.edu.pk>
2021-07-15 09:57:34 +0500

    portfolio section done

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,9 +1,56 @@
 import "./portfolio.scss";
+import PortfolioList from "../PortfolioList/PortfolioList";
+import { useState, useEffect } from "react";
+import { web, desktop } from "../../data";
 
-export default function portfolio() {
+export default function Portfolio() {
+
+    const [selected, setSelected] = useState("web");
+    const [data, setData] = useState([]);
+    const list = [
+        {
+            id: "web",
+            title: "Web App"
+        },
+        {
+            id: "desktop",
+            title: "Desktop App"
+        }
+    ]
+
+    useEffect(() => {
+        switch(selected){
+            case "web":
+                setData(web);
+                break;
+            case "desktop":
+                setData(desktop);
+                break;
+            default:
+                setData(web);
+        }
+    }, [selected])
+
     return (
         <div className="portfolio" id="portfolio">
-            
+            <h1>Portfolio</h1>
+            <ul>
+                {list.map((item) => (
+                    <PortfolioList key={item.id} id={item.id} title={item.title} active={selected === item.id} setSelected={setSelected} />
+                ))}
+            </ul>
+            <div className="container">
+                {data.map((d) => (
+                    <div className="item" key={d.id}>
+                        <img src={d.img} alt={d.title} />
+                        <div className="overlay">
+                            <h3>{d.title}</h3>
+                            <p>{d.desc}</p>
+                        </div>
+                    </div>    
+                ))}
+                
+            </div>
         </div>
     )
 }
---
+++ src/data.js
@@ -0,0 +1,47 @@
+export const web = [
+    {
+        id: 1,
+        title: "Medics",
+        desc: "Online Drug Store",
+        img: "assets/web/medics.png"
+    },
+    {
+        id: 2,
+        title: "Baigro",
+        desc: "Social Networking Website",
+        img: "assets/web/social.png"
+    },
+    {
+        id: 3,
+        title: "Elixir",
+        desc: "Web Templates Selling Website",
+        img: "assets/web/elixir.png"
+    },
+    {
+        id: 4,
+        title: "Hamid Blog's",
+        desc: "Personal Blogging Website",
+        img: "assets/web/hamidBlogs.png"
+    },
+    {
+        id: 5,
+        title: "Backeddia",
+        desc: "Online Shopping Website",
+        img: "assets/web/backeddia.png"
+    },
+    {
+        id: 6,
+        title: "Rambets",
+        desc: "Match schedule Displayer",
+        img: "assets/web/rambets.png"
+    }
+]
+
+export const desktop = [
+    {
+        id: 1,
+        title: "Project Insaniyat",
+        desc: "A Bank App",
+        img: "assets/desktop/insaniyat.png"
+    }
+]
