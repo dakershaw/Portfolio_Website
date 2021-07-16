4eafc109eded646dae8a7b3ef9f48172dd837123
hamidayb <hayub.bese18seecs@seecs.edu.pk>
2021-07-16 10:27:14 +0500

    Work Section Done

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,9 +1,36 @@
 import "./work.scss";
+import {works} from "../../data";
+import { useState } from "react";
 
-export default function work() {
+export default function Work() {
+
+    const [curSlide, setCurSlide] = useState(0);
+
+    const handleClick = (way) => {
+        way === "left" ? setCurSlide(curSlide > 0 ? curSlide - 1 : 2) : setCurSlide(curSlide < works.length - 1 ? curSlide + 1 : 0)
+    }
+
     return (
         <div className="work" id="work">
-            
+            <div className="slider" style={{transform: `translateX(-${curSlide*100}vw)`}}>
+                {works.map((d) => (
+                    <div key={d.id} className="container">
+                        <div className="item">
+                            <div className="left">
+                                <div className="leftContainer">
+                                    <h2>{d.title}</h2>
+                                    <p id="desc">{d.desc}</p>
+                                    <p id="date">{d.date}</p>
+                                </div>
+                            </div>
+                            <div className="right">
+                                <img src={d.img} alt="Innovation Cup" />
+                            </div>
+                        </div>
+                    </div>))}
+            </div>
+            <img src="assets/arrow.png" className="arrow left" alt="arrow" onClick={() => handleClick("left")} />
+            <img src="assets/arrow.png" className="arrow right" alt="arrow" onClick={() =>  handleClick("right")} />
         </div>
     )
 }
--- src/data.js
+++ src/data.js
@@ -45,3 +45,41 @@ export const desktop = [
         img: "assets/desktop/insaniyat.png"
     }
 ]
+
+export const works = [
+    {
+        id: 1,
+        title: "Team Lead Innovation Cup",
+        desc: "Innovation cup is an event organized by ACM empowered by different organization that provides internship to the best Web Developer, Graphic Designer and Machine Learning Engineer",
+        date: "11/11/2020 - 02/02/2021",
+        img: "assets/work/innovation.jpg"
+    },
+    {
+        id: 2,
+        title: "Team Lead CodeChef",
+        desc: "Innovation cup is an event organized by ACM empowered by different organization that provides internship to the best Web Developer, Graphic Designer and Machine Learning Engineer",
+        date: "11/11/2020 - 02/02/2021",
+        img: "assets/work/innovation.jpg"
+    },
+    {
+        id: 3,
+        title: "Team Lead 360 Evluation",
+        desc: "Innovation cup is an event organized by ACM empowered by different organization that provides internship to the best Web Developer, Graphic Designer and Machine Learning Engineer",
+        date: "11/11/2020 - 02/02/2021",
+        img: "assets/work/innovation.jpg"
+    },
+    {
+        id: 4,
+        title: "Member of OC of MLH Local Hack Day",
+        desc: "Innovation cup is an event organized by ACM empowered by different organization that provides internship to the best Web Developer, Graphic Designer and Machine Learning Engineer",
+        date: "11/11/2020 - 02/02/2021",
+        img: "assets/work/innovation.jpg"
+    },
+    {
+        id: 5,
+        title: "Member of OC of DevFest 2019",
+        desc: "Innovation cup is an event organized by ACM empowered by different organization that provides internship to the best Web Developer, Graphic Designer and Machine Learning Engineer",
+        date: "11/11/2020 - 02/02/2021",
+        img: "assets/work/innovation.jpg"
+    }
+]
